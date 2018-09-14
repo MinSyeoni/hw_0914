db457013ecd3e19cf22c19f70704c7c91607cbdb
MinSyeoni <33422760+MinSyeoni@users.noreply.github.com>
2018-09-14 22:51:16 +0900

    2

--- character_move.py
+++ character_move.py
@@ -4,29 +4,43 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-x = 0
+x = 20
 y = 90
 frame = 0
-while (x < 800):
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
-    x += 5
 
-    if (x == 770):
-        x = 770
-        y+=5
-
-    if (y >= 570):
-        y = 570
-        x -= 5
-
-    if (x == 0 and y==570):
-        y -=5
-
-    delay(0.05)
+while (True):
+    while (x < 800 - 20):
+        clear_canvas()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x = x + 2
+        delay(0.01)
+    while (y < 600 - 40):
+        clear_canvas()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y = y + 2
+        delay(0.01)
+    while (x > 0 + 20):
+        clear_canvas()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x = x - 2
+        delay(0.01)
+    while (y > 0 + 90):
+        clear_canvas()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        y = y - 2
+        delay(0.01)
     get_events()
 
 close_canvas()
